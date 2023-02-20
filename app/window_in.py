52d7880c556c8c477c22847be222e8d7eecb6afc
tuyen1601 <tuyen160100@gmail.com>
2023-02-20 17:28:39 +0700

    20/2

--- app/window_in.py
+++ app/window_in.py
@@ -1,12 +1,43 @@
 import cv2
 import sys
+import pytz
+import os
 from datetime import datetime
 sys.path.append("../")
 
 from PyQt5.QtWidgets import QMainWindow, QFileDialog
 from PyQt5.QtGui import QPixmap
 from PyQt5 import uic
+
 from lpr.lprecg import LPRecognizer
+
+from pymongo import MongoClient
+cluster = "mongodb://localhost:27017"
+client = MongoClient(cluster)
+db = client.lpr
+in_collection = db.in_collection
+
+UPLOAD_DIR = "/mnt/c/Users/tuyen/Desktop/Project/Do_an/LPR_App/upload"
+
+
+def uploadFile(image, file_name):
+    if not os.path.exists(UPLOAD_DIR):
+        os.mkdir(UPLOAD_DIR)
+
+    timeTerm: str = datetime.now(pytz.timezone('Asia/Ho_Chi_Minh')) \
+        .strftime('%Y%m%d%H%M%S')
+    imageName: str = f'{timeTerm}-{os.path.basename(file_name)}'
+    imagePath: str = os.path.join(UPLOAD_DIR, imageName)
+    cv2.imwrite(imagePath, image)
+
+    return imagePath
+
+
+def send2In(filePath, textLPR):
+    dbIn = {"Image Path": filePath, "Licence Plate": textLPR}
+    in_collection.insert_one(dbIn)
+
+    return dbIn
 
 
 class IN(QMainWindow):
@@ -24,10 +55,13 @@ class IN(QMainWindow):
         self.labelImage.setPixmap(QPixmap(file_name))
 
         image = cv2.imread(file_name)
+        filePath = uploadFile(image, file_name)
         list_txt, scores = self.lprecognizer.infer(image)
         if scores:
             text = list_txt[scores.index(max(scores))]
         else:
             text = ''
         self.txtLPR.setText(text)
+
+        dbIn = send2In(filePath, text)
         
